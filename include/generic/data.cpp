b9c8fbae3e9723e1858c333d26ebc1be5bfd37e9
dokutan <54861821+dokutan@users.noreply.github.com>
2021-04-24 16:46:03 +0200

    Add M702 PID to mouse_generic::_c_all_pids

--- include/generic/data.cpp
+++ include/generic/data.cpp
@@ -29,24 +29,25 @@ std::set< uint16_t > mouse_generic::_c_all_vids = {
 	0x04d9, // all known mice with generic support have the same VID
 };
 std::set< uint16_t > mouse_generic::_c_all_pids = {
-	0xfc4d, // M908 Impact
+	0xfa56, // M801 Mammoth
+	0xfc0f, // M990 Legend
+	0xfc15, // M702 Phoenix
 	0xfc2a, // M709 Tiger
 	0xfc30, // M711 Cobra (FPS)
-	0xfc39, // M715 Dagger
-	0xfc41, // M990 Legend Chroma/RGB
-	0xfc0f, // M990 Legend
-	0xfc4f, // M719 Invader
-	0xfc42, // M802 Titanoboa 2
-	0xfc5e, // 2858 (?)
-	0xfc58, // 2805 (?)
-	0xfc61, // 2850 (?)
-	0xfc40, // M901 Perdition (3)
-	0xfa56, // M801 Mammoth
-	0xfc56, // M801 Mammoth (?)
-	0xfc49, // M910 Ranger
-	0xfc5f, // M998-RGB and M808-RGB (?)
 	0xfc38, // M607 Griffin
-	0xfc3f // (?)
+	0xfc39, // M715 Dagger
+	0xfc3f, // (?)
+	0xfc40, // M901 Perdition (3)
+	0xfc41, // M990 Legend Chroma/RGB
+	0xfc42, // M802 Titanoboa 2
+	0xfc49, // M910 Ranger
+	0xfc4d, // M908 Impact
+	0xfc4f, // M719 Invader
+	0xfc56, // M801 Mammoth (?)
+	0xfc58, // 2805 (?)
+	0xfc5e, // 2858 (?)
+	0xfc5f, // M998-RGB and M808-RGB (?)
+	0xfc61  // 2850 (?)
 };
 
 // Names of the physical buttons
